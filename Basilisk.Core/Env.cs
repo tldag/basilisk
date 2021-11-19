4d0a4d8579f2d1d64d633151126d429c7532e2a7
rhjoerg <rhjoerg@gmail.com>
2021-11-19 06:50:25 +0100

    Added Basilisk.Tests and Basilisk.Core.Env

---
+++ Basilisk.Core/Env.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Basilisk.Core
+{
+    /// <summary>
+    /// Environment support.
+    /// </summary>
+    public static class Env
+    {
+        /// <summary>
+        /// Shorthand for <code>Environment.GetEnvironmentVariable(key) ?? string.Empty</code>
+        /// </summary>
+        /// <param name="variableName">The variable name to search for.</param>
+        /// <returns>The value of the environment variable or the empty string if there is no such value.</returns>
+        public static string GetEnvironmentVariable(string variableName)
+            => Environment.GetEnvironmentVariable(variableName) ?? string.Empty;
+
+        /// <summary>
+        /// Gets the expanded environment variable for the given variable name.
+        /// </summary>
+        /// <param name="variableName">The variable name to search for.</param>
+        /// <returns>The expanded value of the environment variable or the empty string if there is no such value.</returns>
+        public static string GetExpandedEnvironmentVariable(string variableName)
+            => Environment.ExpandEnvironmentVariables(GetEnvironmentVariable(variableName));
+
+        /// <summary>
+        /// Current/working directory.
+        /// </summary>
+        public static DirectoryInfo CurrentDirectory
+        {
+            get => new(Environment.CurrentDirectory);
+        }
+
+        /// <summary>
+        /// Environment variable <code>PATH</code> converted to enumerable. Contains existing directories only.
+        /// </summary>
+        public static IEnumerable<DirectoryInfo> SearchPath
+        {
+            get
+            {
+                yield return CurrentDirectory;
+
+                foreach (string entry in GetExpandedEnvironmentVariable("PATH").Split(Path.PathSeparator))
+                {
+                    if (Directory.Exists(entry))
+                        yield return new(entry);
+                }
+            }
+        }
+    }
+}
---
+++ Basilisk.Tests/Core/EnvTests.cs
@@ -0,0 +1,42 @@
+﻿using Basilisk.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Basilisk.Tests.Core
+{
+    /// <summary>
+    /// Tests for <code>Basilisk.Core.Env</code>.
+    /// </summary>
+    [TestClass]
+    public class EnvTests
+    {
+        /// <summary>
+        /// TestCurrentDirectory.
+        /// </summary>
+        [TestMethod]
+        public void TestCurrentDirectory()
+        {
+            string expected = new DirectoryInfo(".").FullName;
+            string actual = Env.CurrentDirectory.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// TestSearchPath.
+        /// </summary>
+        [TestMethod]
+        public void TestSearchPath()
+        {
+            string expected = new DirectoryInfo(".").FullName;
+            List<string> actuals = Env.SearchPath.Select(s => s.FullName).ToList();
+
+            Assert.IsTrue(actuals.Contains(expected));
+            Assert.IsTrue(actuals.Count > 1);
+        }
+    }
+}
