aa90c9a095909f3031535042f8275bfdfd59a5fb
rhjoerg <rhjoerg@gmail.com>
2021-11-20 15:14:43 +0100

    Added Basilisk.Collections

---
+++ Basilisk.Collections/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Basilisk.Collections
+{
+    /// <summary>
+    /// Array extensions.
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Creates a shallow copy of the given array.
+        /// </summary>
+        /// <typeparam name="T">Element type.</typeparam>
+        /// <param name="source">Source array</param>
+        /// <returns>Shallow copy.</returns>
+        public static T[] Copy<T>(T[] source)
+            => source.AsEnumerable().ToArray();
+    }
+}
---
+++ Basilisk.Collections/IDictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Basilisk.Collections
+{
+    /// <summary>
+    /// <c>IDictionary</c> extensions.
+    /// </summary>
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Creates a shallow clone of the given dictionary.
+        /// </summary>
+        /// <typeparam name="TKey">Key type.</typeparam>
+        /// <typeparam name="TValue">Value type.</typeparam>
+        /// <param name="source">Source.</param>
+        /// <returns>Shallow clone.</returns>
+        public static Dictionary<TKey, TValue> Clone<TKey, TValue>(IDictionary<TKey, TValue> source) where TKey : notnull
+            => new(source);
+    }
+}
